ecb56cb3c92fe1e6539eff7bc1efa41be6b1a6f2
user1-git-1 <Andilsdk480@gmail.com>
2025-04-27 19:10:11 -0400

    Add hello_world.tx

---
+++ jee.java
@@ -0,0 +1,11 @@
+
+    public class Sequence {
+        public static void main(String[] args) {
+            // Boucles pour générer toutes les séquences de quatre chiffres
+            for (int i = 0; i <= 9999; i++) {
+                
+                System.out.printf("%04d\n", i);
+            }
+        }
+    }
+    
